e5caacbc77c0e9b4b954d2ce664da36bbd36f517
Celest24 <atienocelestineokoth@gmail.com>
2023-01-02 18:43:55 +0300

    printing lower case letters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- Prints the alphabet using put char function in lower case
+ * Return: value set to 0
+ */
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lowercase <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+	putchar('\n');
+	return (0);
+}
