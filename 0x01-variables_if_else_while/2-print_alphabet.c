09999df19414788972a0002ead560cf2da76b309
Celest24 <atienocelestineokoth@gmail.com>
2023-01-02 18:51:23 +0300

    task 2 commit aagain

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 /**
- * main- Prints the alphabet using put char function in lower case
- * Return: value set to 0
+ * main- Prints out the alphabet in lower case
+ * Return: 0
  */
 int main(void)
 {
-	char lower_case;
-	lower_case = 'a'
+	char l_case;
 
-	while ( lowercase <= 'z')
-	{
-		putchar(lower_case++);
-	}
+	l_case = 'a';
+	do {
+		putchar(l_case++);
+	} while (l_case <= 'z');
 	putchar('\n');
 	return (0);
 }
