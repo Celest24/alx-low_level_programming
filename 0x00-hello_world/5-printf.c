0b6a22771633f12fba5ad21af82aee35a9ebd880
Celest24 <atienocelestineokoth@gmail.com>
2023-01-02 15:25:35 +0300

    task 5 and 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main- Enrty point that prints using printf
+ * Return: Always set to 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
